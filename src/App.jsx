0581507c57efa512a4bfd7c60b6e34a133ea648a
HasnatShahriar <shahriarhriday94@gmail.com>
2024-03-19 11:52:26 +0600

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h4>Alhamdulillah</h4>
+      <div className="avatar placeholder">
+        <div className="bg-neutral text-neutral-content rounded-full w-24">
+          <span className="text-3xl">D</span>
+        </div>
+      </div>
+      <div className="avatar online placeholder">
+        <div className="bg-neutral text-neutral-content rounded-full w-16">
+          <span className="text-xl">AI</span>
+        </div>
+      </div>
+      <div className="avatar placeholder">
+        <div className="bg-neutral text-neutral-content rounded-full w-12">
+          <span>SY</span>
+        </div>
+      </div>
+      <div className="avatar placeholder">
+        <div className="bg-neutral text-neutral-content rounded-full w-8">
+          <span className="text-xs">UI</span>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
